1e4303a31f8126d895a4f2adb513e05d768ad741
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2023-11-13 12:05:28 +0100

    updated

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
-  if(!exerciseVideos.length) return 'Loading...'
+  if(!exerciseVideos || !exerciseVideos.length) return 'Loading...'
   return (
     <Box sx={{marginTop: { lg: '200px', xs: '20px'}}} p='20px'>
       <Typography variant='h4' mb='33px'>
